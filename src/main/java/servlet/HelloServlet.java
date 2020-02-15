2115b15c358f228723d8f6dd72a8ecd849351f61
TonyChouteau <tony.chouteau@outlook.fr>
2020-02-15 15:04:11 +0100

    commit

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-        name = "MyServlet",
-        urlPatterns = {"/hello"}
+        name = "root",
+        urlPatterns = {"/"}
     )
 public class HelloServlet extends HttpServlet {
 
@@ -24,16 +24,7 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.print("<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN'	'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>" +
-                  "<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>" +
-                  "    <head>" +
-                  "        <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>" +
-                  "        <title>Fluffy Gaming Center</title>" +
-                  "        <link rel='stylesheet' href='style.css' type='text/css' media='screen' charset='utf-8'/>" +
-                  "    </head>" +
-                  "    <body>" +
-                  "    </body>" +
-                  "</html>");
+        out.print("");
         out.flush();
         out.close();
     }
