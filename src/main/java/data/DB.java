005409688d441e02987e99195c9c9b7e7393f33c
Simon LERIN <slerin@enssat.fr>
2020-03-20 11:35:26 +0100

    création connexion BDD v1

--- src/main/java/data/DB.java
+++ src/main/java/data/DB.java
@@ -1,5 +1,28 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DB implements DBInt {
+
+    private Connection connect;
+    private static final DB instance = new DB();
+
+    private DB() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        
+            connect = DriverManager.getConnection("jdbc:mysql://vps.tonychouteau.fr:3306/", "FluffyCorp", "FluffyWord");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     
+
+    public static final DB getInstance() {
+        return instance;
+    }
+
 }
