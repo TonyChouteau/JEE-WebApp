8d702e1f83a34a5cc86237e9fd2f6096a600587c
Tibo <thibaut.gaudier78@gmail.com>
2020-03-20 08:50:58 +0100

    Servlet Admin et classes DB

--- src/main/java/servlet/Admin.java
+++ src/main/java/servlet/Admin.java
@@ -2,31 +2,33 @@ package servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet (
+    name = "admin",
+    urlPatterns = {
+        "/ban",
+        "/unban",
+        "/endGame",
+        "/addGame",
+        "/removeGame"
+    }
+)
 
 public class Admin extends HttpServlet {
 
     private final static long serialVersionUID = 1L;
     
-    private void doProcess(HttpServletRequest req, HttpServletResponse resp)
-    throws ServletException, IOException {
-        System.out.println("bonjour");
-        String pageName = "/profile.jsp";
-        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
-        try {
-            rd.forward(req, resp);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("here" + req.getRequestURI());
-        doProcess(req, resp);
+    private void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        System.out.println("Connexion à Admin avec le path " + req.getRequestURI());
     }
 
+    public void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doProcess(req, resp);
+    }
 }
