3f7b0f8479d5e05fee4264b42b573040a9a10fea
Tibo <thibaut.gaudier78@gmail.com>
2020-03-20 09:16:37 +0100

    get signin ok

--- src/main/java/servlet/Connect.java
+++ src/main/java/servlet/Connect.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,12 +23,49 @@ public class Connect extends HttpServlet {
 
     private final static long serialVersionUID = 1L;
 
-    private void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    private void doProcess(HttpServletRequest req, HttpServletResponse resp, String mode) throws ServletException, IOException {
         String uri = req.getRequestURI();
         System.out.println("Connexion à Connect avec le path " + req.getRequestURI());
+
+        switch (uri) {
+            case "/signin":
+                if (mode == "GET") {
+                    renderSignin(req, resp);
+                } else if (mode == "POST") {
+                    testSignin(req, resp);
+                }
+                break;
+            case "/signup":
+                break;
+            case "/signout":
+                break;
+            case "/editProfile":
+               break;
+            default :
+                System.out.println("URI non reconnue : " + uri);
+                break;
+        }
+    }
+
+    private void renderSignin (HttpServletRequest req, HttpServletResponse resp) {
+        String pageName = "/signin.jsp";
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
+        try {
+            rd.forward(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testSignin (HttpServletRequest req, HttpServletResponse resp) {
+
     }
 
     public void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doProcess(req, resp);
+        doProcess(req, resp, "GET");
+    }
+
+    public void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doProcess(req, resp, "POST");
     }
 }
