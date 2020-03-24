5db63c6081bfb84aac653d5690763639f54c40cf
dixvies <divi.eflamm@gmail.com>
2020-03-24 20:19:04 +0100

    v1 CurrentGames

---
+++ src/main/java/data/GameLine.java
@@ -0,0 +1,18 @@
+package data;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+public class GameLine{
+    public int idUser;
+    public int game;
+    public Date gameBeginD;
+    public Date gameEndD;
+
+    GameLine(int idUser, int game){
+        this.idUser = idUser;
+        this.game = game;
+        this.gameBeginD = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+        this.gameEndD = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+    }
+}
