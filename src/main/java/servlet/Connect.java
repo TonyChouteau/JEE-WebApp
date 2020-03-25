24a18d234bca48cb65a2f6d48ebc6c1e71e77af8
Tibo <thibaut.gaudier78@gmail.com>
2020-03-25 17:06:15 +0100

    Servlets

--- src/main/java/servlet/Connect.java
+++ src/main/java/servlet/Connect.java
@@ -8,6 +8,7 @@ import java.text.SimpleDateFormat;
 import java.sql.Date;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,73 +24,64 @@ public class Connect extends HttpServlet {
         String uri = req.getRequestURI();
         System.out.println("Connexion à Connect avec le path " + req.getRequestURI());
 
-        switch (uri) {
-            case "/signin":
-                if (mode.equals("GET"))
-                    getSignin(req, resp);
-                else if (mode.equals("POST"))
-                    postSignin(req, resp);
-                break;
-            case "/signup":
-                if (mode.equals("GET"))
-                    getSignup(req, resp);
-                else if (mode.equals("POST"))
-                    postSignup(req, resp);
-                break;
-            case "/signout":
-                if (mode.equals("GET"))
-                    getSignout(req, resp);
-                else if (mode.equals("POST"))
-                    postSignout(req, resp);
-                break;
-            case "/editProfile":
-                if (mode.equals("GET"))
-                    getEditProfile(req, resp);
-                else if (mode.equals("POST"))
-                    postEditProfile(req, resp);
-               break;
-            default :
-                System.out.println("URI non reconnue : " + uri);
-                break;
+        try {
+
+            switch (uri) {
+                case "/signin":
+                    if (mode.equals("GET"))
+                        getSignin(req, resp);
+                    else if (mode.equals("POST"))
+                        postSignin(req, resp);
+                    break;
+                case "/signup":
+                    if (mode.equals("GET"))
+                        getSignup(req, resp);
+                    else if (mode.equals("POST"))
+                        postSignup(req, resp);
+                    break;
+                case "/signout":
+                    if (mode.equals("GET"))
+                        getSignout(req, resp);
+                    else if (mode.equals("POST"))
+                        postSignout(req, resp);
+                    break;
+                case "/editProfile":
+                    if (mode.equals("GET"))
+                        getEditProfile(req, resp);
+                    else if (mode.equals("POST"))
+                        postEditProfile(req, resp);
+                   break;
+                default :
+                    System.out.println("URI non reconnue : " + uri);
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    private void postEditProfile(HttpServletRequest req, HttpServletResponse resp) {
+    private void postEditProfile(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
        //TODO parce que j'ai la flemme
 
     }
 
-    private void getEditProfile(HttpServletRequest req, HttpServletResponse resp) {
-        String pageName = "/WEB-INF/views/profile.jsp";
-        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
-        try {
-            rd.forward(req, resp);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void getEditProfile(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        displayPage(req, resp, "/WEB-INF/views/profile.jsp");
     }
 
-    private void postSignout(HttpServletRequest req, HttpServletResponse resp) {
+    private void postSignout(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.getSession().invalidate();
-        try {
-            resp.sendRedirect("/home");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        resp.sendRedirect("/home");
     }
 
-    private void getSignout(HttpServletRequest req, HttpServletResponse resp) {
+    private void getSignout(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.getSession().invalidate();
-        try {
-            resp.sendRedirect("/home");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        resp.sendRedirect("/home");
     }
 
-    private void postSignup(HttpServletRequest req, HttpServletResponse resp) {
-
+    private void postSignup(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    // TODO Check les paramètres !!!
         String username = req.getParameter("username");
         Date d = new Date(42);
         int uid = db.signup(username, req.getParameter("email"), req.getParameter("password"), d);
@@ -98,33 +90,17 @@ public class Connect extends HttpServlet {
             HttpSession session = req.getSession();
             session.setAttribute("username", username);
             session.setAttribute("uid", uid);
-            try {
-                resp.sendRedirect("/home");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            resp.sendRedirect("/home");
         } else {
-            String pageName = "/WEB-INF/views/signin.jsp";
-            RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
-            try {
-                rd.forward(req, resp);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            displayPage(req, resp, "/WEB-INF/views/signin.jsp");
         }
     }
 
-    private void getSignup(HttpServletRequest req, HttpServletResponse resp) {
-        String pageName = "/WEB-INF/views/signup.jsp";
-        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
-        try {
-            rd.forward(req, resp);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void getSignup(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        displayPage(req, resp, "/WEB-INF/views/signup.jsp");
     }
 
-    private void postSignin (HttpServletRequest req, HttpServletResponse resp) {
+    private void postSignin (HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         String username = req.getParameter("username");
 
@@ -134,31 +110,15 @@ public class Connect extends HttpServlet {
             HttpSession session = req.getSession();
             session.setAttribute("username", username);
             session.setAttribute("uid", uid);
-            try {
-                resp.sendRedirect("/home");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            resp.sendRedirect("/home");
 
         } else {
-            String pageName = "/WEB-INF/views/signin.jsp";
-            RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
-            try {
-                rd.forward(req, resp);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            displayPage(req, resp, "/WEB-INF/views/signin.jsp");
         }
     }
 
-    private void getSignin(HttpServletRequest req, HttpServletResponse resp) {
-        String pageName = "/WEB-INF/views/signin.jsp";
-        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
-        try {
-            rd.forward(req, resp);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void getSignin(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        displayPage(req, resp, "/WEB-INF/views/signin.jsp");
     }
 
     public void doGet (HttpServletRequest req, HttpServletResponse resp) {
@@ -168,5 +128,20 @@ public class Connect extends HttpServlet {
     public void doPost (HttpServletRequest req, HttpServletResponse resp) {
         System.out.println("ok boomer");
         doProcess(req, resp, "POST");
+    }
+
+    private void displayPage (HttpServletRequest req, HttpServletResponse resp, String page) throws IOException{
+
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(page);
+        try {
+            rd.forward(req, resp);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String checkParam (HttpServletRequest req, String param) {
+        //TODO
+        return null;
     }
 }
--- src/main/java/servlet/Players.java
+++ src/main/java/servlet/Players.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,55 +15,85 @@ public class Players extends HttpServlet {
     private void doProcess(HttpServletRequest req, HttpServletResponse resp, String mode) throws ServletException, IOException {
         String uri = req.getRequestURI();
         System.out.println("Connexion à Players avec le path " + req.getRequestURI());
-        
-        switch (uri) {
-            case "/play":
-                if (mode.equals("GET"))
-                    getPlayer(req, resp);
-                else if (mode.equals("POST"))
-                    postPlayer(req, resp);
-                break;
-            case "/currentGames":
-                if (mode.equals("GET"))
-                    getListPlayers(req, resp);
-                else if (mode.equals("POST"))
-                    postListPlayers(req, resp);
-                break;
-            case "/getCurrentGames":
-                if (mode.equals("GET"))
-                    getGetListPlayers(req, resp);
-                else if (mode.equals("POST"))
-                    postGetListPlayers(req, resp);
-                break;
-            default :
-                System.out.println("URI non reconnue : " + uri);
-                break;
+
+        try {
+            switch (uri) {
+                case "/play":
+                    if (mode.equals("GET")) {
+                        getPlayer(req, resp);
+                    }
+                    else if (mode.equals("POST")) {
+                        postPlayer(req, resp);
+                    }
+                    break;
+                case "/currentGames":
+                    if (mode.equals("GET")) {
+                        getListPlayers(req, resp);
+                    }
+                    else if (mode.equals("POST")) {
+                        postListPlayers(req, resp);
+                    }
+                    break;
+                case "/getCurrentGames":
+                    if (mode.equals("GET")) {
+                        getGetListPlayers(req, resp);
+                    }
+                    else if (mode.equals("POST")) {
+                        postGetListPlayers(req, resp);
+                    }
+                    break;
+                default :
+                    System.out.println("URI non reconnue : " + uri);
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     
 
-    private void postGetListPlayers(HttpServletRequest req, HttpServletResponse resp) {
+    private void postGetListPlayers(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
     }
 
-    private void getGetListPlayers(HttpServletRequest req, HttpServletResponse resp) {
+    private void getGetListPlayers(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // TODO récupérer la liste des joueurs
     }
 
-    private void postListPlayers(HttpServletRequest req, HttpServletResponse resp) {
+    private void postListPlayers(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
     }
 
-    private void getListPlayers(HttpServletRequest req, HttpServletResponse resp) {
+    private void getListPlayers(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        displayPage(req, resp, "/players.jsp");
     }
 
-    private void postPlayer(HttpServletRequest req, HttpServletResponse resp) {
+    private void postPlayer(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
     }
 
-    private void getPlayer(HttpServletRequest req, HttpServletResponse resp) {
+    private void getPlayer(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        displayPage(req, resp, "player.jsp");
     }
+
+
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doProcess(req, resp, "GET");
     }
     public void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doProcess(req, resp, "POST");
+    }
+
+
+    private void displayPage (HttpServletRequest req, HttpServletResponse resp, String page) throws IOException{
+
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(page);
+        try {
+            rd.forward(req, resp);
+            resp.sendError(HttpServletResponse.SC_OK);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
     }
 }
