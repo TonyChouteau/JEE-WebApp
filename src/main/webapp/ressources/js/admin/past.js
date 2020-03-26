62054845e0f43bd74097009ff0fb9c59ed2728b7
TonyChouteau <tony.chouteau@outlook.fr>
2020-03-26 21:36:48 +0100

    added current and past list

---
+++ src/main/webapp/ressources/js/admin/past.js
@@ -0,0 +1,43 @@
+
+function loadPast(){
+    //fetch to get past games
+    
+    let content = `
+        <h3 class="game-list-title">
+            Current Games
+        </h3>
+    `;
+
+    fetch('/getPastGames').then(response => {
+        return response.json()
+    }).then(data => {
+        let adding = ""
+        for (let i in data){
+            console.log(i)
+            adding+=`
+                <div class="gamelist-item">
+                    <div class="game current-id">
+                        `+data[i].pid+`
+                    </div>
+                    <div class="verticalSeparator"></div>
+                    <div class="game past-info">
+                        `+data[i].nameUser+`
+                    </div>
+                    <div class="verticalSeparator"></div>
+                    <div class="game past-info">
+                        `+data[i].nameGame+`
+                    </div>
+                    <div class="verticalSeparator"></div>
+                    <div class="game past-info">
+                        `+data[i].dateDebut+`
+                    </div>
+                    <div class="verticalSeparator"></div>
+                    <div class="game past-info">
+                        `+data[i].dateFin+`
+                    </div>
+                </div>
+            `;
+        }
+        document.getElementById("games-list").innerHTML = content+adding;
+    })
+}
