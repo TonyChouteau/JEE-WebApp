d4af79cc1fd9e1756a9103fd0c2739369198be88
TonyChouteau <tony.chouteau@outlook.fr>
2020-03-27 00:28:23 +0100

    adding online info on game list

--- src/main/webapp/ressources/js/games.js
+++ src/main/webapp/ressources/js/games.js
@@ -7,29 +7,38 @@ function loadGames(){
             Available Games 
         </h3>
     `
+    let adding = ""
 
     fetch('/getGames').then(response => {
         return response.json()
     }).then(data => {
-        let adding = ""
-        for (let i in data){
-            if (data[i].available === true) {
-                adding+=`
-                    <a href=/play?gid=`+ data[i].gid +`>
-                    <div class="gamelist-item">
-                        <div class="game game-id">
-                            `+data[i].gid+`
+        fetch('/getNbPlayers').then(response => {
+            return response.json()
+        }).then(data2 => {
+            for (let i in data){
+                if (data[i].available === true) {
+                    let online = data2[data[i].gid]==undefined?0:data2[data[i].gid];
+                    adding+=`
+                        <a href=/play?gid=`+ data[i].gid +`>
+                        <div class="gamelist-item">
+                            <div class="game game-id">
+                                `+data[i].gid+`
+                            </div>
+                            <div class="verticalSeparator"></div>
+                            <div class="game game-name">
+                                `+data[i].name+`
+                            </div>
+                            <div class="verticalSeparator"></div>
+                            <div class="game game-online">
+                                `+online+` online
+                            </div>
                         </div>
-                        <div class="verticalSeparator"></div>
-                        <div class="game game-name">
-                            `+data[i].name+`
-                        </div>
-                    </div>
-                    </a>
-                `;
+                        </a>
+                    `;
+                }
             }
-        }
-        document.getElementById("games-list").innerHTML = content+adding;
+            document.getElementById("games-list").innerHTML = content+adding;
+        })
     })
     
     
